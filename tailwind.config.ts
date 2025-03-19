9206742c4400d70d42a42acd854bff8ae3f35adc
qwer0114 <rnalgh0114@gmail.com>
2025-03-19 22:57:51 +0900

    💄 style: color desing system 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          maru: '#002968',
+          egg: '#F0F2F6',
+        },
+        monotone: {
+          black: '#19181d',
+          'black-50': 'rgba(25, 24, 29, 0.5)',
+          white: '#ffffff',
+        },
+        grayscale: {
+          'gray-90': '#252528',
+          'gray-80': '#3D3D42',
+          'gray-70': '#56565D',
+          'gray-60': '#6E6E77',
+          'gray-50': '#888891',
+          'gray-40': '#A2A2A9',
+          'gray-30': '#BDBDC2',
+          'gray-20': '#D7D7DA',
+          'gray-10': '#F2F2F3',
+        },
+        calendar: {
+          saturday: '#057CFF',
+          sunday: '#FE4A4A',
+        },
+        error: '#F60000',
       },
       fontFamily: {
         sans: ['Pretendard', 'sans-serif'],
-      }
+      },
     },
   },
   plugins: [],
