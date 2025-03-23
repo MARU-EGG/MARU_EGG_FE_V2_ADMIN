9e80724f1a6ccc15cca7d0368f51bc708cfa82fb
Choi JunHo <duekzim@gmail.com>
2025-03-23 18:21:40 +0900

    🧼 clean: 테스트 코드 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,9 @@
 import { API_PATH } from '@/stores/querys/api-path';
 
 export default function Home() {
-  const submit = async () => {
-    const data = await fetch(`${API_PATH.schedules}`, {
-      method: 'get',
-    }).then((res) => {
-      return res.json();
-    });
-
-    console.log(data);
-  };
-
   return (
     <>
-      <button onClick={submit}>테스트용 콘솔 확인 버튼</button>
+      <div>홈</div>
     </>
   );
 }
