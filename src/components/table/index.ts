a6060f51c3a87ef64373ff8acd8473be93fdcc92
qwer0114 <rnalgh0114@gmail.com>
2025-03-29 18:09:11 +0900

    ✨ feat: table 컴포넌트 barrel 파일 사용

---
+++ src/components/table/index.ts
@@ -0,0 +1,5 @@
+export { default as Table } from '@/components/table/table';
+export { default as TableHead } from '@/components/table/head';
+export { default as TableBody } from '@/components/table/body';
+export { default as TableRow } from '@/components/table/row';
+export { default as TableCell } from '@/components/table/cell';
