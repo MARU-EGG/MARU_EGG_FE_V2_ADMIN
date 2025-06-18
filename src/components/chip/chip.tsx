6bec670314b0d2d4123e75c6768b933bcfcbeb7a
Choi JunHo <duekzim@gmail.com>
2025-06-19 00:14:56 +0900

    ✨ feat: chip 컴포넌트 추가

---
+++ src/components/chip/chip.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { cn } from '@/utils/style';
+
+type ChipProps = {
+  label: string;
+  status: 'success' | 'error';
+};
+
+function Chip({ label, status }: ChipProps) {
+  const baseStyle = 'w-fit rounded px-3 py-1.5 text-xs font-semibold ring-1 ring-inset';
+
+  const statusStyle = cn(
+    status === 'error' && 'ring-error bg-error/10 text-error',
+    status === 'success' && 'ring-success bg-success/10 text-success',
+  );
+
+  return <div className={cn(baseStyle, statusStyle)}>{label}</div>;
+}
+
+export default Chip;
