6b9bf1dceb7c6ea2e1e149e4182039e58ee5dd47
qwer0114 <rnalgh0114@gmail.com>
2026-02-21 14:50:23 +0900

    feat: spring, llm server axios instance 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -4,3 +4,11 @@ export const open_api_axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_PUBLIC_API_ADDRESS,
   timeout: 20000,
 });
+
+export const llm_server_api_axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_LLM_SERVER_API_ADDRESS,
+});
+
+export const spring_server_api_axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SPRING_SERVER_API_ADDRESS,
+});
