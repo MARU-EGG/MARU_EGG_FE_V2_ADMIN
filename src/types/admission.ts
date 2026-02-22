ce82e59b2ce42265b15c57e685b63b5e8b70dab2
qwer0114 <rnalgh0114@gmail.com>
2026-02-22 15:07:56 +0900

    feat: 학과 type 및 상수 정의

---
+++ src/types/admission.ts
@@ -0,0 +1,15 @@
+export const ADMISSIONS = { SUSI: '수시', JEONGSI: '정시', PYEONIP: '편입' } as const;
+
+export type AdmissionType = keyof typeof ADMISSIONS;
+export type AdmissionLabel = (typeof ADMISSIONS)[AdmissionType];
+
+export const ADMISSIONS_CATEGORIES = {
+  ADMISSION_GUIDE: '모집요강',
+  PASSING_RESULT: '입시결과',
+  PAST_QUESTION: '기출문제',
+  CAMPUS_LIFE: '대학생활',
+  INTERVIEW_PRACTICAL: '면접/실기',
+} as const;
+
+export type AdmissionCategoryType = keyof typeof ADMISSIONS_CATEGORIES;
+export type AdmissionCategoryLabel = (typeof ADMISSIONS_CATEGORIES)[AdmissionCategoryType];
