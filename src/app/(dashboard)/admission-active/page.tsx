54317d2986e4c32b923173345cc93e84fea505cc
qwer0114 <rnalgh0114@gmail.com>
2026-02-22 15:06:44 +0900

    feat: 전형 활성화 ui 정의

--- src/app/(dashboard)/admission-active/page.tsx
+++ src/app/(dashboard)/admission-active/page.tsx
@@ -1,3 +1,26 @@
-export default function AdmissionActive() {
-  return <div>admission-active</div>;
+import { getDocuments } from '@/api/llm/documents';
+import { getAdmissionsStatus } from '@/api/swagger/admission';
+import AdmissionActiveClient from '@/app/(dashboard)/admission-active/admission-active-client';
+import { QueryClient } from '@tanstack/react-query';
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+
+export default async function AdmissionActive() {
+  const queryClient = new QueryClient();
+
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ['admissionsStatus'],
+      queryFn: getAdmissionsStatus,
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ['documents', '수시', '모집요강'],
+      queryFn: () => getDocuments({ type: '수시', category: '모집요강' }),
+    }),
+  ]);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <AdmissionActiveClient />
+    </HydrationBoundary>
+  );
 }
