37114aca2c3d2c59ed25d926305b11c8770ca5a8
qwer0114 <rnalgh0114@gmail.com>
2026-02-25 17:55:04 +0900

    feat: 문서관련 api 정의

--- src/api/llm/documents.ts
+++ src/api/llm/documents.ts
@@ -1,6 +1,6 @@
 import { llm_server_api_axiosInstance } from '@/api/instance';
 import { AdmissionCategoryLabel, AdmissionLabel } from '@/types/admission';
-import { DocumentResponse } from '@/types/document';
+import { DocumentResponse, UploadDocumentParams } from '@/types/document';
 
 export async function getDocuments({
   type,
@@ -11,5 +11,28 @@ export async function getDocuments({
 }): Promise<DocumentResponse> {
   const params = { type, category };
   const response = await llm_server_api_axiosInstance.get('/retrieve_documents', { params });
+  return response.data;
+}
+
+export async function uploadDocument({ type, category, pdf_file, page_gap }: UploadDocumentParams) {
+  const formdata = new FormData();
+  formdata.append('type', type);
+  formdata.append('category', category);
+  formdata.append('pdf_file', pdf_file);
+  formdata.append('page_gap', '3');
+
+  const response = await llm_server_api_axiosInstance.post('/upload_pdf/', formdata, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return response.data;
+}
+
+export async function deleteDocument({ type, category }: { type: AdmissionLabel; category?: AdmissionCategoryLabel }) {
+  const params = { type, category };
+  const response = await llm_server_api_axiosInstance.delete('/delete_documents', {
+    params,
+  });
   return response.data;
 }
